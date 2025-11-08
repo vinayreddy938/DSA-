f4161c803a53ec58d925dc1e820ec98692687310
Vinay Reddy <vinaykumarreddycse@gmail.com>
2025-11-08 22:09:44 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (78.05%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let n = s.length-1;
+    let count = 0;
+    while(n>=0){
+        if(s[n]!=" "){
+            count++;
+        }
+        else if(count > 0){
+            break;
+        }
+
+        n--;
+    }
+    return count;
+    
+};
