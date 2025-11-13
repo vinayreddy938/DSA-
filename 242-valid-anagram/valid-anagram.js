148e7d8acd516377ab7686a07dc90da69cca0f08
Vinay Reddy <vinaykumarreddycse@gmail.com>
2025-11-13 23:08:02 +0530

    Time: 13 ms (65.33%) | Memory: 57.8 MB (60.10%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,7 +4,25 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-        return( (s.split("").sort().join("")) === ( t.split("").sort().join(""))) ;
+    if(s.length!=t.length){
+        return false;
+    }
+    const map = new Map();
+    for(let i=0;i<s.length;i++){
+        if(map[s[i]]){
+            ++map[s[i]];
+        }else{
+            map[s[i]] = 1;
+        }
+    }
+    for(let i=0;i<t.length;i++){
+        if(!map[t[i]] || map[t[i]]<0){
+            return false;
+        }else{
+            --map[t[i]];
+        }
+    }
+    return true;
 
     
 };
