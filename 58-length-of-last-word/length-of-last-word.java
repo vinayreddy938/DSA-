1762338c266d9c6c893356e7e12c8180692b9323
Vinay Reddy <vinaykumarreddycse@gmail.com>
2026-06-05 20:54:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (81.77%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,7 +1,17 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-         s = s.trim();
-         String[] arr = s.split(" ");
-         return arr[arr.length-1].length();
+        int i=s.length()-1;
+        int count =0;
+        while(i>=0 && s.charAt(i)==' ' ){ 
+            i--;
+
+        }
+        
+       while(i>=0 && s.charAt(i)!=' ' ){
+        count++;
+
+        i--;
+       }
+        return count;
     }
 }
